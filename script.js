bb82f01f3f78bbe5213e603b52014891e52561ff
Lucas <lbrandof@gmail.com>
2021-03-11 17:35:54 -0300

    Corrigindoi Bug na abertura do Content

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function showhidecontent() {
-    if(document.querySelector('#content').style.display == 'none') {
+    if(document.querySelector('#content').classList.contains('contentclose')) {
+        document.querySelector('#content').classList.remove('contentclose');
         document.querySelector('#content').classList.add('contentopen');
     } else {
         document.querySelector('#content').classList.remove('contentopen');
