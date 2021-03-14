ffee17ccce287d3420c9e920f0d82ae70147f0f6
Lucas <lbrandof@gmail.com>
2021-03-14 15:34:20 -0300

    Alterações do botão de abertura da div content

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-function showhidecontent() {
-    if(document.querySelector('#content').classList.contains('contentclose')) {
-        document.querySelector('#content').classList.remove('contentclose');
-        document.querySelector('#content').classList.add('contentopen');
-    } else {
-        document.querySelector('#content').classList.remove('contentopen');
-        document.querySelector('#content').classList.add('contentclose');
-    }    
-}
+function openbutton() {
+    document.querySelector('#content-button').classList.toggle("clicar");
+    document.querySelector('#content').classList.toggle("contentopen");
+    document.querySelector('#content').classList.toggle("contentclose");
+  }
